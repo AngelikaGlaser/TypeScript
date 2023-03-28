8104ba557fadd543b2983126ec309bcb6a868b5d
AngelikaGlaser <angelikaglaser2001@gmail.com>
2023-03-28 13:12:29 +0300

    Added various routers and modules.

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import stringsController from "./controllers/strings";
+import productController from "./controllers/products";
+import productsListController from "./controllers/productlist";
 
 const app: Express = express();
 
@@ -8,6 +10,8 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use('/', stringsController);
+app.use('/', productsListController);
+app.use('/toode', productController);
 
 app.listen(3000,() => {
     console.log(`[server]: Server is running at http://localhost:3000`);
